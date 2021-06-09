e547e8371de6d03944210bf2d156228f190f12df
deathxexm <51823045+deathxexm@users.noreply.github.com>
2021-06-09 20:26:14 +0800

    + Bug Fix: New codes for MySQL R/W (WIP)

--- DBConnect.java
+++ DBConnect.java
@@ -8,22 +8,26 @@ public class DBConnect {
 
     private static Connection connection;
     private static Statement st;
+    private static PreparedStatement prepst;
     private static ResultSet rs;
-    private static final String path = "jdbc:mysql://localhost:3306/bananabun";
+    private static final String path = "jdbc:mysql://localhost:3306/issuetrackerdata";
 
     public DBConnect() {
         try {
-            connection = DriverManager.getConnection(path, "root", "12345678");
+            connection = DriverManager.getConnection(path, "root", "root");
             st = connection.createStatement();
         } catch (SQLException ex) {
             System.out.println(ex);
         }
     }
 
-        public void newProject(long ID, String Name, String IssuesArrJSONString) {
+    public void newProject(long ID, String Name, String IssuesArrJSONString) {
         try {
-            String query = "INSERT INTO projects (ID, Name, Issues) values (\'" + ID + "\', \'" + Name + "\', \'" + IssuesArrJSONString + "\')";
-            st.executeUpdate(query);
+            prepst = connection.prepareStatement("INSERT INTO projects (ID,Name,Issues) values (?,?,?)");
+            prepst.setLong(1, ID);
+            prepst.setString(2, Name);
+            prepst.setString(3, IssuesArrJSONString);
+            prepst.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e);
         }
@@ -48,12 +52,13 @@ public class DBConnect {
         JSONParser parser = new JSONParser();
         String name = ""; String issues;
         try {
-            String query = "SELECT * FROM projects WHERE ID = " + ID +"";
-            rs = st.executeQuery(query);
-            
-            ID = rs.getLong("ID");
-            name = rs.getString("Name");
-            issues = rs.getString("Issues");
+            prepst = connection.prepareStatement("SELECT * FROM projects WHERE ID=?");
+            prepst.setLong(1, ID);
+            rs = prepst.executeQuery();
+            rs.next();
+            ID = rs.getLong(1);
+            name = rs.getString(2);
+            issues = rs.getString(3);
             issuesArr = (JSONArray) parser.parse(issues);
             
             return new Project(ID,name,issuesArr);
@@ -66,8 +71,11 @@ public class DBConnect {
     
     public void setProject(long ID, String issuesJsonString) {
         try {
-            String query = "UPDATE projects SET Issues = " +issuesJsonString +" WHERE ID = " +ID +"";
-            rs = st.executeQuery(query);
+            String query = "UPDATE projects SET Issues = ? WHERE ID = ?";
+            prepst = connection.prepareStatement(query);
+            prepst.setString(1, issuesJsonString);
+            prepst.setLong(2, ID);
+            prepst.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Fail to update a project with new data.");
             System.out.println("Detail: " +e);
@@ -76,8 +84,11 @@ public class DBConnect {
     
     public void newUser(long ID, String Username, String Password) {
         try {
-            String query = "INSERT INTO projects (ID, Name, Issues) values (\'" + ID + "\', \'" + Username + "\', \'" + Password + "\')";
-            rs = st.executeQuery(query);
+            prepst = connection.prepareStatement("INSERT INTO users (ID,Username,Password) values (?,?,?)");
+            prepst.setLong(1, ID);
+            prepst.setString(2, Username);
+            prepst.setString(3, Password);
+            prepst.executeUpdate();
         } catch (SQLException e) {
             System.out.println("Fail to register new user data into MySQL database");
             System.out.println("Detail: " +e);
@@ -101,9 +112,11 @@ public class DBConnect {
     public User getUser(long ID) {
         
         try {
-            String query = "SELECT * FROM users WHERE ID = " +ID +"";
-            rs = st.executeQuery(query);
+            prepst = connection.prepareStatement("SELECT * FROM users WHERE ID=?");
+            prepst.setLong(1, ID);
             
+            rs = prepst.executeQuery();
+            rs.next();
             ID = rs.getLong("ID");
             String Username = rs.getString("Username");
             String Password = rs.getString("Password");
@@ -120,72 +133,71 @@ public class DBConnect {
         System.out.println("No need modify user data.");
     }
     //SELECT EXISTS(SELECT * FROM yourTableName WHERE yourCondition);
-    /*
-    public void newReaction(int userID, int comID, int interaction) {
-        try {
-            String query = "INSERT INTO reaction (userID , comID, interaction) values (\'" + userID + "\', \'" + comID + "\', \'" + interaction + "\')";
-            st.executeUpdate(query);
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-    }
+//    public void newReaction(int userID, int comID, int interaction) {
+//        try {
+//            String query = "INSERT INTO reaction (userID , comID, interaction) values (\'" + userID + "\', \'" + comID + "\', \'" + interaction + "\')";
+//            st.executeUpdate(query);
+//        } catch (SQLException ex) {
+//            System.out.println(ex);
+//        }
+//    }
+//
+//    public void newComment(int issueID, String userName, int comID, String comText, String timeStamp) {
+//        try {
+//            String query = "INSERT INTO comment (issueID, userName, comID, comText, timeStamp, reaction) values "
+//                    + issueID + "\'+ \'" + userName + "\', \'" + comID + "\', \'" + comText + "\', \'" + timeStamp + "\')";
+//            st.executeUpdate(query);
+//        } catch (SQLException ex) {
+//            System.out.println(ex);
+//        }
+//    }
+//
+//    public static void getComment(int issueID) {
+//        int order_num = 1;
+//        int happy = 0, sad = 0, angry = 0, confused = 0, thankful = 0;
+//        try {
+//            String query = "SELECT comment.comID, comment.userName, comment.timeStamp, comment.comText,"
+//                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 1 AND reaction.comID = comment.comID) as happy,"
+//                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 2 AND reaction.comID = comment.comID) as sad,"
+//                    +//interactions are tabulated
+//                    "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 3 AND reaction.comID = comment.comID) as angry,"
+//                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 4 AND reaction.comID = comment.comID) as confused,"
+//                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 5 AND reaction.comID = comment.comID) as thankful"
+//                    + " FROM comment"//from the comment table
+//                    + " WHERE issueID = " + issueID + "";//whereby the issueID is the one passed through the args
+//
+//            rs = st.executeQuery(query);
+//            System.out.println("Comments");
+//            System.out.println("----------");
+//
+//            while (rs.next()) {
+//                String userName = rs.getString("userName");
+//                String timeStamp = rs.getString("timeStamp");
+//                String comText = rs.getString("comText");
+//                String comID = rs.getString("comID");
+//                happy = rs.getInt("happy");
+//                sad = rs.getInt("sad");
+//                angry = rs.getInt("angry");
+//                confused = rs.getInt("confused");
+//                thankful = rs.getInt("thankful");
+//
+//                System.out.println("#" + order_num + "\t Created on: " + timeStamp
+//                        + "\tBy: " + userName + "\tcomID: " + comID + "\n" + comText);
+//                System.out.println("Reactions: Happy (" + happy + ") | Sad (" + sad + ")"
+//                        + "| Angry (" + angry + ") | Confused (" + confused + ") | Thankful (" + thankful + ")");
+//                System.out.println("");
+//                order_num++;
+//
+//            }
+//            if (order_num == 1) {
+//                System.out.println("No comment");
+//            }
+//
+//        } catch (SQLException ex) {
+//            System.out.println(ex);
+//        }
+//    }
 
-    public void newComment(int issueID, String userName, int comID, String comText, String timeStamp) {
-        try {
-            String query = "INSERT INTO comment (issueID, userName, comID, comText, timeStamp, reaction) values "
-                    + issueID + "\'+ \'" + userName + "\', \'" + comID + "\', \'" + comText + "\', \'" + timeStamp + "\')";
-            st.executeUpdate(query);
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-    }
-
-    public static void getComment(int issueID) {
-        int order_num = 1;
-        int happy = 0, sad = 0, angry = 0, confused = 0, thankful = 0;
-        try {
-            String query = "SELECT comment.comID, comment.userName, comment.timeStamp, comment.comText,"
-                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 1 AND reaction.comID = comment.comID) as happy,"
-                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 2 AND reaction.comID = comment.comID) as sad,"
-                    +//interactions are tabulated
-                    "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 3 AND reaction.comID = comment.comID) as angry,"
-                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 4 AND reaction.comID = comment.comID) as confused,"
-                    + "(SELECT COUNT(interaction) FROM reaction WHERE interaction = 5 AND reaction.comID = comment.comID) as thankful"
-                    + " FROM comment"//from the comment table
-                    + " WHERE issueID = " + issueID + "";//whereby the issueID is the one passed through the args
-
-            rs = st.executeQuery(query);
-            System.out.println("Comments");
-            System.out.println("----------");
-
-            while (rs.next()) {
-                String userName = rs.getString("userName");
-                String timeStamp = rs.getString("timeStamp");
-                String comText = rs.getString("comText");
-                String comID = rs.getString("comID");
-                happy = rs.getInt("happy");
-                sad = rs.getInt("sad");
-                angry = rs.getInt("angry");
-                confused = rs.getInt("confused");
-                thankful = rs.getInt("thankful");
-
-                System.out.println("#" + order_num + "\t Created on: " + timeStamp 
-                        + "\tBy: " + userName + "\tcomID: " + comID + "\n" + comText);
-                System.out.println("Reactions: Happy (" + happy + ") | Sad (" + sad + ")"
-                        + "| Angry (" + angry + ") | Confused (" + confused + ") | Thankful (" + thankful + ")");
-                System.out.println("");
-                order_num++;
-
-            }
-            if (order_num == 1) {
-                System.out.println("No comment");
-            }
-
-        } catch (SQLException ex) {
-            System.out.println(ex);
-        }
-    }
-    */
     /*reactionDraft
     public static void getReaction(int comID) {
         int happy = 0, sad = 0, angry = 0, confused = 0, thankful = 0;
