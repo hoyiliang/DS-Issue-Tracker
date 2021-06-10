58537fb0431328a57e3c6c53dc345c4895a59411
Yeezy-5145 <76953378+Yeezy-5145@users.noreply.github.com>
2021-06-10 17:02:30 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -31,6 +31,10 @@ public class Main {
     public static Stack<UndoRedo> issueDescRedo = new Stack<>();
     public static Stack<UndoRedo> commentUndo = new Stack<>();
     public static Stack<UndoRedo> commentRedo = new Stack<>();
+    public static Scanner input = new Scanner(System.in);
+    public static DBConnect connection = new DBConnect();
+    public static String comText, timeStamp, userName;
+    public static int issueID, comID, userID, interaction;
 
     //Setting up JSON READ
     public static void main(String[] args) throws IOException, ParseException, FileNotFoundException {
@@ -1135,5 +1139,39 @@ public class Main {
             }
         }
         return projectIssues;
+    }
+    
+        public static void newTime(int comID) {
+        timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm").format(new java.util.Date());
+    }
+
+/* main menu segment for comments & reactions *reminder to integrate
+        System.out.println("Enter\n'r' to react \n'c' to comment \n'help' for more commands \nany key to exit");
+        String x = input.nextLine();
+        if (x.equalsIgnoreCase("c")) {
+            inputComment();
+            viewComment();
+        } if (x.equalsIgnoreCase("r")) {
+            inputReaction();
+        }*/
+
+    public static void inputComment() {
+        System.out.println("Enter: ");
+        comText = input.nextLine();
+        connection.newComment(issueID, userName, comID, comText, timeStamp);
+    }
+
+    public static void viewComment() {
+        System.out.println("");
+        connection.getComment(issueID);
+    }
+
+    public static void inputReaction() {
+        input.nextLine();
+        System.out.println("Which comment are you recating to? *Type in the comID*");
+        comID = input.nextInt();
+        System.out.println("React \n '1' for happy  \n'2' for sad \n'3' for angry \n'4' for confused \n'5' for thankful");
+        interaction = input.nextInt();
+        connection.newReaction(userID, comID, interaction);
     }
 }
